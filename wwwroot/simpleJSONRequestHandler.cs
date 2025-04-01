e4f8bac9bf326dc7bf1e8ee6985cd32f68e02fe9
WithTempo <connor.ryan.mcgaughey@proton.me>
2025-04-01 17:47:35 -0500

    adding functionality, preparing to transition to standardized

---
+++ wwwroot/simpleJSONRequestHandler.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomWebServer.wwwroot
+{
+    public class simpleHttpServer2
+    {
+        
+    }
+}
