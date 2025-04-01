63303d9df0b119831b26132d1295fb60d93a52ec
WithTempo <connor.ryan.mcgaughey@proton.me>
2025-04-01 13:05:22 -0500

    fixed errors and bugs

--- obj/Debug/net8.0/CustomWebServer.AssemblyInfo.cs
+++ obj/Debug/net8.0/CustomWebServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomWebServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef8bdf322db1a13595147af998700d3fe830de14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+421e6e931ae8320e71985d65305e906a6d853949")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomWebServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomWebServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
