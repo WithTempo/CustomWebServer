12e5841a96eaac2fb860ca0041799f60d6285910
WithTempo <connor.ryan.mcgaughey@proton.me>
2025-04-02 17:01:12 -0500

    moving on

--- obj/Debug/net8.0/CustomWebServer.AssemblyInfo.cs
+++ obj/Debug/net8.0/CustomWebServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomWebServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+421e6e931ae8320e71985d65305e906a6d853949")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb5b413c5151b19cc44e21b8195100e7646e697f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomWebServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomWebServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
